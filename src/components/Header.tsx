6b308a88ef32a02e205f78bbcdf534bb85d6f5f6
henry-bao <henrybao@uw.edu>
2025-04-30 14:26:56 -0400

    nothing important

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,24 +11,24 @@ const Header: React.FC = () => {
             <div className="top-nav">
                 <div className="container">
                     <div className="top-nav-left">
-                        <a href="#" className="top-nav-link">
+                        <a href="/" className="top-nav-link">
                             <i className="icon-phone"></i> 1-877-424-2449
                         </a>
-                        <a href="#" className="top-nav-link">
+                        <a href="/" className="top-nav-link">
                             <i className="icon-globe"></i> ENG
                         </a>
-                        <a href="#" className="top-nav-link">
+                        <a href="/" className="top-nav-link">
                             <i className="icon-currency"></i> USD
                         </a>
                     </div>
                     <div className="top-nav-right">
-                        <a href="#" className="top-nav-link">
+                        <a href="/" className="top-nav-link">
                             <i className="icon-user"></i> SIGN IN
                         </a>
-                        <a href="#" className="top-nav-link">
+                        <a href="/" className="top-nav-link">
                             <i className="icon-member"></i> JOIN
                         </a>
-                        <a href="#" className="top-nav-link join-btn">
+                        <a href="/" className="top-nav-link join-btn">
                             JOIN NOW
                         </a>
                     </div>
@@ -46,22 +46,22 @@ const Header: React.FC = () => {
                     </div>
                     <nav className="nav-menu">
                         <ul>
-                            <a href="#" className="nav-item">
+                            <a href="/" className="nav-item">
                                 <i className="icon-book"></i> Book
                             </a>
-                            <a href="#" className="nav-item">
+                            <a href="/" className="nav-item">
                                 <i className="icon-manage"></i> Manage a Booking
                             </a>
-                            <a href="#" className="nav-item">
+                            <a href="/" className="nav-item">
                                 <i className="icon-hotels"></i> Hotels & Resorts
                             </a>
-                            <a href="#" className="nav-item">
+                            <a href="/" className="nav-item">
                                 <i className="icon-offers"></i> Special Offers
                             </a>
-                            <a href="#" className="nav-item">
+                            <a href="/" className="nav-item">
                                 <i className="icon-about"></i> About Us
                             </a>
-                            <a href="#" className="nav-item">
+                            <a href="/" className="nav-item">
                                 <i className="icon-rewards"></i> IHG One Rewards
                             </a>
                             <a href={isDemoRoute ? '/' : '/demo'} className={`nav-item security-demo`}>
